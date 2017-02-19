ba5ecfa2560b903b9c16fdc1b2313f05652880c4
pradeep burugu <pradeepburugu@pradeeps-MacBook-Pro.local>
2017-02-19 17:26:15 +0530

    add get request/response

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,12 +2,43 @@ import Vapor
 
 let drop = Droplet()
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+//get
+drop.get("hello") { _ in
+    return "Hello World!"
 }
 
-drop.resource("posts", PostController())
+//nesting
+drop.get("welcome", "hello") { request in
+   return "Welcome, Hello World!"
+}
+
+//with parameter
+drop.get("name",":name") { request in
+  if let name = request.parameters["name"]?.string {
+    return "Welcome, \(name)"
+  }
+  return "Unable to retrieve the key:name from JSON.\n \(request)"
+}
+
+drop.get("/profession/:profession") { request in
+  if let profession = request.parameters["profession"]?.string {
+    return "\(profession)"
+  }
+  return ""
+}
+
+//Type-safe parameters
+drop.get("age", Int.self) { request, age in
+  return "\(age)"
+}
+
+//multiple-parametes
+drop.get("/name/:name/age/:age") { request in
+  if let name =  request.parameters["name"]?.string, let age = request.parameters["age"]?.string {
+    return "\(name), \(age)"
+  }
+  return ""
+}
+
 
 drop.run()
